d4e3e382ca1db708b2a6d378982c7cd3f6654e83
Abishek Thapa <111527019+Abishek2022@users.noreply.github.com>
2023-03-22 22:02:22 +0545

    Change all file

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="roadmap.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+</head>
+<body>
+	
+	<script>
+window.onscroll = function() {myFunction()};
+
+var nevigation = document.getElementById("myHeader");
+var sticky = nevigation.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    nevigation.classList.add("sticky");
+  } else {
+    nevigation.classList.remove("sticky");
+  }
+}
+</script>
+<div class="topbtn">
+			<button class="button loginbutton">Log in</button>
+			<button class="button register">Creat Account</button>
+			
+</div>
+
+	<div class="nevigation" id="myHeader">
+	<div class="navbar">
+  <div class="subnav">
+    <button class="subnavbtn">WHAT TO EXPECT<i class="#"></i></button>
+   
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">LEADERS<i class="#"></i></button>
+   
+  </div> 
+  <div class="subnav">
+    <button class="subnavbtn">SERMONS<i class="#"></i></button>
+   
+  </div>
+  <div class="subnav">
+    <button class="subnavbtn">MISSION<i class="#"></i></button>
+  
+  </div>
+    <div class="subnav">
+    <button class="subnavbtn">MINISTRY PARTNERS<i class="#"></i></button>
+   
+  </div>
+   <div class="subnav">
+    <button class="subnavbtn">OUR BELIEFS<i class="#"></i></button>
+   
+  </div>
+   <div class="subnav">
+    <button class="subnavbtn">EVENTS<i class="#"></i></button>
+  
+  </div>
+   <div class="subnav">
+    <button class="subnavbtn">GIVE<i class="#"></i></button>
+    
+  </div>
+  
+</div>
+ <a href="#"><img src="logo4.JPG" style="width: 110px; height: 95px; margin-top: -62px; margin-left:68px; position: absolute; " ></a>
+			 
+</div>
+	
+			
+
+		
+<div class="title"><h1>SAMARPAN CHURCH</h1></div>
+<div  class="first">
+	<div class="first-content">
+	<div class="mphoto fphoto1"><div class="uphoto p1"><img src="uphoto1.JPG"></div></div>
+	<div class="mtext  ftext1"><h1>Wirship</h1><p>Worship is designed for the people of God, to stimulate their praises and to feed their souls. Yet the Gospel is so woven into every part that anyone can understand</p></div>
+	<div class="mphoto fphoto2"><div class="uphoto p2"><img src="uphoto2.JPEG"></div></div>
+		<div class="mtext ftext2"><h1>Discipleship</h1><p>Worship is designed for the people of God, to stimulate their praises and to feed their souls. Yet the Gospel is so woven into every part that anyone can understand<</p></div>
+	<div class="mphoto fphoto3"><div class="uphoto p3"><img src="uphoto3.JPG"></div></div>
+	<div class="mtext ftext3"><h1>Preaching</h1><p>Worship is designed for the people of God, to stimulate their praises and to feed their souls. Yet the Gospel is so woven into every part that anyone can understand<</p></div>
+	<div class="mphoto  fphoto4"><div class="uphoto p4"><img src="uphoto4.JPG"></div></div>
+	<div class="mtext ftext4"><h1>Music</h1><p>Worship is designed for the people of God, to stimulate their praises and to feed their souls. Yet the Gospel is so woven into every part that anyone can understand<</p></div>
+</div>
+</div>
+	  
+
+
+	    <div class="footer">
+    	  <a href="#"><img src="logo4.JPG" style="width: 137px; height: 125px; margin-top: 113px; margin-left:220px; position: relative; " ></a>
+    	  <div class="footer-content">
+    	<form  class="footer-nev0">
+	  	<input type="text" name="name" placeholder="Name">
+	  	<input type="text" name="email" placeholder="Email">
+	  	<input type="submit" name="submit" value="Subscribe">
+    	 
+    		</form> 
+    	<div class="footer-nev1">
+
+    		<h3>SITE MAP</h3>
+				<a href="#">About Us</a>
+				<a href="#">Sermons</a>
+				<a href="#">Events</a>
+				<a href="#">Give</a>
+    	</div>
+    	<div class="footer-nev2">
+    	  <h3>SUPPORT</h3>
+				<a href="#">Privacy Policy</a>
+				<a href="#">Terms and Condition</a>
+				
+    	</div>
+    	<div class="footer-nev3">
+    	  <h3>FOLLOW ON</h3>
+			<a class="active" href="https://www.facebook.com/prem.pun.900"><i class="fa fa-facebook"></i></a> 
+		  <a href="#"><i class="fa fa-instagram"></i></a> 
+		  <a href="#"><i class="fa fa-twitter"></i></a> 
+		  <a href="#"><i class="fa fa-telegram"></i></a>
+    	</div>
+    	<div class="footer-nev4">
+    	  <h3>CONTACT US</h3>
+				<a href="#"><i class="fa fa-envelope"></i> truthoftrinity2020</a>
+    	</div>
+    	</div>
+    </div>
+
+</body>
+</html>
